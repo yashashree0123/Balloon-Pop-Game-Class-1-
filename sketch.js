12b27aa8374c31db9eb5e1f742395349eff5068d
yashashree0123 <117700343+yashashree0123@users.noreply.github.com>
2024-03-10 10:34:20 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+var background1
+function preload(){
+    background1=loadImage("game-boilerplate-latest-main 2/Balloon Game.gif")
+}
+function setup(){
+    createCanvas(900,900)
+}
+function draw(){
+    background(background1)
+}
